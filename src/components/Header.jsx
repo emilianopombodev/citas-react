213b3ccf62ec3dbda01f8e59599252f6c0b51e4c
unknown <emilianopombodev@gmail.com>
2022-10-06 12:50:11 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+
+  return (
+    <>
+    <h1 className='font-black text-5xl text-center md:w-2/2 w-1/2 mx-auto'>
+      Seguimiento Pacientes {""}
+       <spam className='text-indigo-600'>Veterinaria</spam>
+      </h1>
+    </>
+
+    
+  )
+}
+
+export default Header
